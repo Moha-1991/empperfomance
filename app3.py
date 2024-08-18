1b56b3d478209d135e1ef6a936a6dcfb5c6bcae0
Moha-1991 <mohamedyare454@gmail.com>
2024-08-18 10:30:29 +0300

    first commit

--- app3.py
+++ app3.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt
+import seaborn as sns
 import warnings
 # Ignore warnings
 warnings.filterwarnings('ignore')
